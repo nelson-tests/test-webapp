f3dbfb84a760936b4d1e7673fd5eb82d13df2000
Nelson Nobre <technobre@gmail.com>
2023-07-06 20:46:12 +0100

    Update

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,14 @@
+using System.Reflection;
+
+var version = Assembly.GetEntryAssembly()?.GetName()?.Version;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Welcome to Minimal API");
+app.MapGet("/", () => $"Welcome to Minimal API - {version}");
 app.MapGet("/machine", () => Environment.MachineName);
-app.MapGet("/version", () => Environment.Version);
+app.MapGet("/version", () => version);
+app.MapGet("/framework-version", () => Environment.Version);
 app.MapGet("/os", () => $"{Environment.OSVersion.Platform} - {Environment.OSVersion.VersionString}");
 
 app.Run();
